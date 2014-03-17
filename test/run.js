dbb79c14e61ee979fc26f5b9581dc3f8229b4be9
jeromew <jerome.wagner@m4x.org>
2014-03-17 19:15:02 +0000

    Download upstream jade test cases via Makefile

--- test/run.js
+++ test/run.js
@@ -7,23 +7,16 @@ var fs = require('fs');
 var assert = require('assert');
 var ty = require('then-yield');
 var uglify = require('uglify-js');
-var download = require('gethub');
 var Promise = require('promise');
 var jade = require('../');
-var upstream_version = require('../node_modules/jade/package.json').version;
-var upstream_jade = require('jade');
+var jade_upstream = require('jade');
 
 // define custom filter which is necessary for upstream tests
-upstream_jade.filters['custom-filter'] = function (str, options) {
+jade_upstream.filters['custom-filter'] = function (str, options) {
   assert(str === 'foo bar');
   assert(options.foo === 'bar');
   return 'bar baz';
 };
-
-// download upstream jade tests (same version as jade dependency in package.json)
-it('download upstream jade test cases', function(done) {
-  download('visionmedia', 'jade', upstream_version, __dirname + '/upstream', done);
-});
 
 // create output directory
 try {
@@ -35,7 +28,7 @@ try {
 }
 
 // upstream test cases
-var cases = fs.readdirSync('test/upstream/test/cases').filter(function(file){
+var cases = fs.readdirSync('test/jade/test/cases').filter(function(file){
   return ~file.indexOf('.jade');
 }).map(function(file){
   return file.replace('.jade', '');
@@ -44,10 +37,10 @@ var cases = fs.readdirSync('test/upstream/test/cases').filter(function(file){
 cases.forEach(function(test){
   var name = test.replace(/[-.]/g, ' ');
   async(name, function* () {
-    var path = 'test/upstream/test/cases/' + test + '.jade';
+    var path = 'test/jade/test/cases/' + test + '.jade';
     var str = fs.readFileSync(path, 'utf8');
-    var html = fs.readFileSync('test/upstream/test/cases/' + test + '.html', 'utf8').trim().replace(/\r/g, '');
-    var fn = jade.compile(str, { filename: path, pretty: true, basedir: 'test/upstream/test/cases' });
+    var html = fs.readFileSync('test/jade/test/cases/' + test + '.html', 'utf8').trim().replace(/\r/g, '');
+    var fn = jade.compile(str, { filename: path, pretty: true, basedir: 'test/jade/test/cases' });
     var actual = yield fn({title: 'Jade'});
 
     fs.writeFileSync(__dirname + '/output/' + test + '.html', actual)
@@ -85,3 +78,4 @@ generators.forEach(function(test){
     JSON.stringify(actual.trim()).should.equal(JSON.stringify(html));
   });
 });
+
